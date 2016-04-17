4457025efa72a6d8a97429e09c35a3f5520c37d5
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-04-17 15:08:35 +0700

    import move

--- source/package.d
+++ source/package.d
@@ -2,7 +2,6 @@ module rtree;
 
 import std.traits;
 debug import std.stdio;
-import gfm.math.box: box2i; // If place this import to top of file @disabled error is gone
 
 class RTree(Node, bool isWritable)
 {
@@ -24,6 +23,7 @@ class RTree(Node, bool isWritable)
         }
     }
 
+    import gfm.math.box: box2i; // If place this import to top of file @disabled error is gone
     alias Box = box2i;
 
     static if(isWritable)
