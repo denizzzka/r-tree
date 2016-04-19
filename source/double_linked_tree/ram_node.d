1787fc1598fac651c40f9ff7070239e3aa966c3d
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-04-19 20:01:38 +0700

    showBranch added

--- source/double_linked_tree/ram_node.d
+++ source/double_linked_tree/ram_node.d
@@ -1,11 +1,12 @@
 module double_tree.ram_node;
 
 import std.container: SList;
+debug import std.stdio;
 
 struct RAMNode(Payload)
 {
     private RAMNode* __parent;
-    debug package bool isLeafNode = false;
+    debug package bool isPayloadNode = false;
 
     private union
     {
@@ -15,7 +16,7 @@ struct RAMNode(Payload)
 
     ref Payload payload() @property
     {
-        debug assert(isLeafNode);
+        debug assert(isPayloadNode);
 
         return __payload;
     }
@@ -65,20 +66,20 @@ struct RAMNode(Payload)
         return __parent;
     }
 
-    RAMNode* addPayloadNode(Payload payload)
+    RAMNode* addPayloadNode(Payload payload) @property
     {
-        debug assert(!isLeafNode);
+        debug assert(!isPayloadNode);
 
         auto n = addNode();
-        n.isLeafNode = true;
+        n.isPayloadNode = true;
         n.payload = payload;
 
         return n;
     }
 
-    RAMNode* addNode()
+    RAMNode* addNode() @property
     {
-        debug assert(!isLeafNode);
+        debug assert(!isPayloadNode);
 
         RAMNode* child = new RAMNode;
         child.__parent = &this;
@@ -86,4 +87,45 @@ struct RAMNode(Payload)
 
         return __children.childrenStorage.front();
     }
+
+    debug static void showBranch(RAMNode* from, size_t depth = 0)
+    {
+        write("Depth=", depth, " Node=", from, " parent=", from.parent, " ");
+
+        if(from.isPayloadNode)
+        {
+            writeln("payload=", from.payload);
+        }
+        else
+        {
+            writeln("parent=", from.parent, " children=", from.children);
+
+            foreach(ref c; from.children[])
+            {
+                showBranch(c, depth + 1);
+            }
+        }
+    }
+
+    //~ void statistic(
+        //~ ref size_t nodesNum,
+        //~ ref size_t leafsNum,
+        //~ ref size_t leafBlocksNum,
+        //~ Node* curr,
+        //~ size_t currDepth = 0
+    //~ ) pure
+    //~ {
+        //~ if(currDepth == depth)
+        //~ {
+            //~ leafBlocksNum++;
+            //~ leafsNum += curr.children.length;
+        //~ }
+        //~ else
+        //~ {
+            //~ nodesNum += curr.children.length;
+
+            //~ foreach(c; curr.children)
+                //~ statistic( nodesNum, leafsNum, leafBlocksNum, c, currDepth+1 );
+        //~ }
+    //~ }
 }
--- source/double_linked_tree/tree.d
+++ source/double_linked_tree/tree.d
@@ -4,12 +4,62 @@ unittest
 {
 	import double_tree.ram_node;
 
-    RAMNode!(ubyte) root;
+    {
+		// simple test
+		RAMNode!ubyte root;
 
-    auto n = root.addNode;
-    auto leaf = n.addPayloadNode(123);
+		auto n = root.addNode;
+		auto leaf = n.addPayloadNode(123);
 
-    assert(leaf.payload == 123);
-    assert(n == leaf.parent);
-    assert(&root == n.parent);
+		assert(leaf.payload == 123);
+		assert(n == leaf.parent);
+		assert(&root == n.parent);
+	}
+
+	{
+		// complex test
+		alias TestType = float;
+
+		alias Node = RAMNode!TestType;
+		Node writable;
+
+		size_t counter;
+		Node curr;
+
+		void recursive(Node* node, size_t currDepth = 0)
+		{
+			foreach(i; 0 .. 3)
+			{
+				if(currDepth < 2)
+				{
+					auto newNode = node.addNode;
+					recursive(newNode, currDepth + 1);
+				}
+				else
+				{
+					node.addPayloadNode(counter + currDepth/10);
+					counter++;
+				}
+			}
+		}
+
+		recursive(&writable);
+
+		debug(double_tree) writable.showBranch(&writable);
+
+		size_t nodes, leafs, leafBlocksNum;
+		//~ writable.statistic(nodes, leafs, leafBlocksNum);
+
+		//~ assert(leafs == 9);
+		//~ // assert(nodes == 13);
+		//~ assert(leafBlocksNum == 6);
+
+		//~ assert(writable.root.boundary == BBox(1, 1, 4, 4));
+
+		//~ auto search1 = BBox(2, 2, 3, 3);
+		//~ auto search2 = BBox(2.1, 2.1, 2.9, 2.9);
+
+		//~ assert(writable.search( search1 ).length == 9);
+		//~ assert(writable.search( search2 ).length == 1);
+	}
 }
